64e9602e4205d322183544e5a0946c272e5ce9b7
fim <n5pghmjdy9@privaterelay.appleid.com>
2026-09-18 14:43:46 +0700

    add UserServiceImpl

--- Co-Working-Space-Equipment-Rental/Co-Working-Space/src/main/java/com/example/roombooking/repository/UserRepository.java
+++ Co-Working-Space-Equipment-Rental/Co-Working-Space/src/main/java/com/example/roombooking/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.roombooking.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.roombooking.domain.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByUsername(String username);
 }
