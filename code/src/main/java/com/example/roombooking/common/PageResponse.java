1bc3b843a47621133aa03f4bd28300b088c170b7
fimlnwza007 <n5pghmjdy9@privaterelay.appleid.com>
2026-09-22 22:31:05 +0700

    Refactor PageResponse class to remove generics
    Removed generic type and related fields from PageResponse class.

--- code/src/main/java/com/example/roombooking/common/PageResponse.java
+++ code/src/main/java/com/example/roombooking/common/PageResponse.java
@@ -1,24 +1,3 @@
-package com.example.roombooking.common;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import java.util.List;
-
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class PageResponse<T> {
-
-    private List<T> content;
-
-    private int page;
-    private int size;
-    private long totalElements;
-    private int totalPages;
-    private boolean last;
-
+public class PageResponse {
 }
